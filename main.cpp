55a32be52d05cd56fdfd7d7d6a5becd321e31748
Tanner Davison <Tboydavison@gmail.com>
2024-12-23 23:05:33 -0700

    Delete your info
    Delete your info

--- main.cpp
+++ main.cpp
@@ -4,11 +4,13 @@
 using std::cout, std::endl, std::string;
 
 int main() {
+
   string name = "Corey Thompson";
   string dob = "11/10/1995";
-  // TODO
   int hash = 1e4;
   cout << name << " " << dob << endl;
   cout << "hash #" << hash << endl;
+
+  // Delete your information ^^
   return 0;
 }
